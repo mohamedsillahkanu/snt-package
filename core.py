916e59721d0bc14bc7193140ebd159ca42babd6d
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2025-04-25 14:33:10 -0500

    Create core.py

---
+++ core.py
@@ -0,0 +1,36 @@
+# snt/core.py
+import pandas as pd
+from pathlib import Path
+
+def concatenate(folder_path):
+    files = Path(folder_path).glob("*.xlsx")
+    df_list = [pd.read_excel(file) for file in files]
+    return pd.concat(df_list, ignore_index=True)
+
+def rename(df, dict_path):
+    name_map = pd.read_excel(dict_path)
+    for i in range(len(name_map)):
+        old = name_map.iloc[i, 0]
+        new = name_map.iloc[i, 1]
+        df.rename(columns={old: new}, inplace=True)
+    return df
+
+def compute(df, compute_path):
+    comp = pd.read_excel(compute_path)
+    for i in range(len(comp)):
+        new_var = comp['new_variable'][i]
+        op = comp['operation'][i]
+        components = [x.strip() for x in comp['components'][i].split(',')]
+
+        if op == "rowsum":
+            df[new_var] = df[components].sum(axis=1, skipna=True)
+        elif op == "subtract":
+            df[new_var] = df[components[0]] - df[components[1]]
+            df[new_var] = df[new_var].clip(lower=0)
+    return df
+
+def split(df, split_path):
+    split_cols = pd.read_excel(split_path)['columns'].tolist()
+    id_vars = [col for col in df.columns if col not in split_cols]
+    return pd.melt(df, id_vars=id_vars, value_vars=split_cols,
+                   var_name='variable', value_name='value')
