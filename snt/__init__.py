7d47a792ea386de6b06838effb39cee2990f5b08
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2025-04-27 12:38:57 -0500

    Update __init__.py

--- snt/__init__.py
+++ snt/__init__.py
@@ -1,3 +1,3 @@
-from .core import rename, concatenate, compute, split, sort, outliers
+from .core import rename, concatenate, compute, split, sort, detect_outliers
 
-__all__ = ['rename', 'concatenate', 'compute', 'split', 'sort', 'outliers']
+__all__ = ['rename', 'concatenate', 'compute', 'split', 'sort', 'detect_outliers']
